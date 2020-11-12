927a7f62ca5de6a4fa3d2f1532bca488edf98e7e
Christ Maël <fomekongchristmael@gmail.com>
2020-11-12 20:06:42 +0100

    Sync bottom navigation with pages

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,12 +1,31 @@
+import 'package:ebmn_mobile/screens/mainScreenChildren/EmergencyScreen.dart';
+import 'package:ebmn_mobile/screens/mainScreenChildren/HistoryScreen.dart';
+import 'package:ebmn_mobile/screens/mainScreenChildren/PatientScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+
+final _pageIndexProvider = StateProvider((_) => 1);
 
 class MainScreen extends HookWidget{
+  final List<HookWidget> pages = [
+    PatientScreen(), EmergencyScreen(),
+    HistoryScreen()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: HookBuilder(
+        builder: (context){
+          final pageIndex = useProvider(_pageIndexProvider).state;
+          return pages[pageIndex];
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){
+          context.read(_pageIndexProvider).state = index;
+        },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.person_pin)
